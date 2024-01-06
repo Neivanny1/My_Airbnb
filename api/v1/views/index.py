ac8e277c6dbc94cebd9707dde1d58e2dcca1e4a8
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-01-06 22:36:52 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Returning json
+Create a route '/status' on onject app_views
 """
 from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route("/status")
+@app_views.route("/status", methods=["GET"])
 def status():
     """
-    Returns status
+    Returns JSON response for REST api health
     """
     status = {"status": "ok"}
     return jsonify(status)
