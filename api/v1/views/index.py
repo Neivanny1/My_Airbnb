9d793f64a692c959024b3ff456a6d3081e5db53e
Mwakazi_eric <123241068+Neivanny1@users.noreply.github.com>
2024-01-08 22:22:41 +0000

    Add files via upload

---
+++ api/v1/views/index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Index
+"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+classes = {"users": "User", "places": "Place", "states": "State",
+           "cities": "City", "amenities": "Amenity",
+           "reviews": "Review"}
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """
+    Routes to status page
+    """
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', methods=['GET'])
+def count():
+    """
+    Retrieves the number of each objects by type
+    """
+    count_dict = {}
+    for cls in classes:
+        count_dict[cls] = storage.count(classes[cls])
+    return jsonify(count_dict)
