0ead8e85c246970b00d22698410afa8eefafeba7
Neivanny1 <nevileneivanny@gmail.com>
2024-01-08 18:56:46 +0000

    fix style error

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 Contains class BaseModel
 """
-
+import inspect
 from datetime import datetime
 import models
 from os import getenv
@@ -20,14 +20,18 @@ else:
 
 
 class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+    """
+    The BaseModel class from which future classes will be derived
+    """
     if models.storage_t == "db":
         id = Column(String(60), primary_key=True)
         created_at = Column(DateTime, default=datetime.utcnow)
         updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """
+        Initialization of the base model
+        """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -48,18 +52,24 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """
+        String representation of the BaseModel class
+        """
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
+        """
+        Updates the attribute 'updated_at' with the current datetime
+        """
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of the instance"""
+        """
+        Returns a dictionary containing all keys/values of the instance
+        """
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
@@ -68,8 +78,18 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
+        frame = inspect.currentframe().f_back
+        func_name = frame.f_code.co_name
+        class_name = ''
+        if 'self' in frame.f_locals:
+            class_name = frame.f_locals["self"].__class__.__name__
+        is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
+        if 'password' in new_dict and not is_fs_writing:
+            del new_dict['password']
         return new_dict
 
     def delete(self):
-        """delete the current instance from the storage"""
+        """
+        delete the current instance from the storage
+        """
         models.storage.delete(self)
--- models/user.py
+++ models/user.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" holds class User"""
+"""
+Holds class User
+"""
+import hashlib
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
@@ -9,15 +12,25 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """Representation of a user """
+    """
+    Representation of a user
+    """
     if models.storage_t == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship(
+            "Place",
+            cascade="all, delete, delete-orphan",
+            backref="user"
+        )
+        reviews = relationship(
+            "Review",
+            cascade="all, delete, delete-orphan",
+            backref="user"
+        )
     else:
         email = ""
         password = ""
@@ -25,5 +38,18 @@ class User(BaseModel, Base):
         last_name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes user"""
+        """
+        Initializes user
+        """
         super().__init__(*args, **kwargs)
+
+    def __setattr__(self, __name: str, __value) -> None:
+        """
+        Sets an attribute of this class to a given value.
+        """
+        if __name == 'password':
+            if type(__value) is str:
+                m = hashlib.md5(bytes(__value, 'utf-8'))
+                super().__setattr__(__name, m.hexdigest())
+        else:
+            super().__setattr__(__name, __value)
